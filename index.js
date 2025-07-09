ac3ade697f29d73dc1efd7153148a1c9c2c7ecd7
Abid Raheem <75900873+AbidVFX@users.noreply.github.com>
2025-07-09 12:19:04 +0500

    modified

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import jsonfile from "jsonfile";
 import moment from "moment";
 import simpleGit  from "simple-git";
 const path = "./data.json";
-const date = moment().format();
+const date = moment().subtract(5, "days").format();
 
 const data = { 
     data: date,
  };
 
-jsonfile.writeFileSync(path, data);
-
+jsonfile.writeFileSync(path, data, () => {
 simpleGit().add([path]).commit(date, {'--date': date}).push();
+});
